f025f661fcf4d53ad6018ef962c53fde79d01849
Mercedes <mercedespontaut@gmail.com>
2022-09-23 10:00:20 -0300

    primero

---
+++ js/main.js
@@ -0,0 +1,16 @@
+sup = `
+<header>                
+        <a class="brand" href="index.html">
+            <img src="./imagenes/brand.png" width="55" alt="logo">Skincare
+        </a>               
+        <nav>            
+            <ul>                      
+                <li><a class="active" href="index.html">Inicio</a></li>
+                <li><a href="#">Cuidados de la piel</a></li>
+                <li><a href="#">Agregar sección</a></li>
+                <li><a href="contacto.html">Contacto</a></li>
+            </ul>    
+        </nav>        
+    </header>
+`
+document.write(sup)
